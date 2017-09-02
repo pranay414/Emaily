3a24739d0a2ffcfcb99175de37096595138fd340
PRANAY ANKIT TIRU <pranayankittiru@PRANAYs-MacBook-Pro.local>
2017-09-02 10:48:54 +0530

    added new route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',(req, res) => {
     res.send({hi: 'there'});
 });
 
+app.get('/user1',(req, res) => {
+    res.send({hi: "hello budy"});
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
