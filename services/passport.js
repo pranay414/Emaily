eb1039ff83d84af9b3a5eb0ce754b28300991d25
PRANAY ANKIT TIRU <pranayankittiru@PRANAYs-MacBook-Pro.local>
2017-09-22 17:48:07 +0530

    did something weird, hope it works

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
 
