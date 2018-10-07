dc8db318e64e9e0493e439ec6ede7763a1c18a35
Pranay Ankit <pranayankit414@gmail.com>
2018-10-07 21:50:42 +0530

    heroku build scripts added

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    // Express will serve production assets
+    // like our main.js, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log("HTTP server running on port", PORT);
 });
